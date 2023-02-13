2dca7754fdc4fc2815904e035668f2369842df15
jdettelback <jdettelback@gmail.com>
2023-02-13 17:43:05 -0500

    working on About page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,49 @@
-import React, {useState} from "react";
+import React from "react";
 
+// const About = () => {
+//   const styles = {
+//     img: {
+//       backgroundImage: "url('../images/football.jpg')",
+//       backgroundRepeat: "no-repeat",
+//       backgroundSize: "cover",
+//       opacity: 0.5,
+//       height: "100vh",
+//       zIndex: -1,
+//     },
+//   };
 
-export default function  About() {
+//   return (
+//     <div>
+//       <main className="flex-row justify-center m-4">
+//         <img
+//           style={styles.img}
+//           href="./images/football.jpg"
+//           src="./images/football.jpg"
+//           alt="football"
+//         ></img>
 
+//         <div>
+//           <h1 className="opacity-100">Information about our Camp</h1>
+//         </div>
+//         <div className="row justify-content-around m-4">
+//           <div className="col col-5 justify-center border border-danger">
+//             Skills
+//           </div>
+//           <div className="col col-6 justify-center border border-info">
+//             Testimonials
+//           </div>
+//         </div>
+//       </main>
+//     </div>
+//   );
+// };
 
-    return (
-        <div>
-        <h1>About Page</h1>
-        </div>
-    )
+// export default About;
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
 }
