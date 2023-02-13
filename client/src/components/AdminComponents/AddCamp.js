4b3f4830d2873a9b23281b01c21977513a23b8a2
pmorris50 <patrickmorris532@gmail.com>
2023-02-13 10:04:08 -0700

    admin page and components

--- client/src/components/AdminComponents/AddCamp.js
+++ client/src/components/AdminComponents/AddCamp.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import FormModal from '../FormModal';
 
 
@@ -10,12 +10,18 @@ const AddCamp = () => {
     //const dateString = date.toLocaleDateString();
 
     return (
-        <FormModal>
-        <form>
-            <input type = "date" value = {date} onChange = {e => setDate(e.target.value)}>
-            </input>
-        </form>
-        </FormModal>
+        <div>
+            <button onClick={() => setShowModal(true)}>Add a Camp</button>
+            <FormModal showModal={showModal} setShowModal={setShowModal}>
+                <form>
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}>
+                    </input>
+                    <input type="text" value={location} onChange={(e) => setLocation(e.target.value)}></input>
+                    <input type="date" value={date} onChange={e => setDate(e.target.value)}>
+                    </input>
+                </form>
+            </FormModal>
+        </div>
     )
 
 }
