f73f0b1ebfd53497f3c7ad702ce86e0cc64e9c19
jdettelback <jdettelback@gmail.com>
2023-02-13 21:31:37 -0500

    styled About page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,49 +1,37 @@
 import React from "react";
+import "./About.css";
 
-// const About = () => {
-//   const styles = {
-//     img: {
-//       backgroundImage: "url('../images/football.jpg')",
-//       backgroundRepeat: "no-repeat",
-//       backgroundSize: "cover",
-//       opacity: 0.5,
-//       height: "100vh",
-//       zIndex: -1,
-//     },
-//   };
+const About = () => {
+  const styles = {
+    
+  };
 
-//   return (
-//     <div>
-//       <main className="flex-row justify-center m-4">
-//         <img
-//           style={styles.img}
-//           href="./images/football.jpg"
-//           src="./images/football.jpg"
-//           alt="football"
-//         ></img>
-
-//         <div>
-//           <h1 className="opacity-100">Information about our Camp</h1>
-//         </div>
-//         <div className="row justify-content-around m-4">
-//           <div className="col col-5 justify-center border border-danger">
-//             Skills
-//           </div>
-//           <div className="col col-6 justify-center border border-info">
-//             Testimonials
-//           </div>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default About;
-
-export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <main className="flex-row justify-center">
+        <img src="../images/football.jpg" alt="football" className="img" />
+        <div>
+          <h1 className="opacity-100 p-4">Information about our Camp</h1>
+        </div>
+        <div className="row justify-content-around m-4">
+          <div className="col col-5 justify-center border border-danger">
+            Skills
+          </div>
+          <div className="col col-5 justify-center border border-info">
+            Testimonials
+          </div>
+        </div>
+      </main>
     </div>
   );
-}
+};
+
+export default About;
+
+// export default function About() {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//     </div>
+//   );
+// }
