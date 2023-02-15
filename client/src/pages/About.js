c5bc22a4fe4176942f648f5a2dd12229733df4f7
val.gee <vguevara0806@gmail.com>
2023-02-15 11:43:04 -0500

    finished rendering all camps and conditionally render a registration button for when the user is logged in

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.css";
-import Auth from '../utils/auth'
+import Auth from '../utils/auth';
+import Camp from '../components/Camp';
 
 const About = () => {
   const styles = {
@@ -10,33 +11,35 @@ const About = () => {
   return (
     <div>
       {Auth.loggedIn() ? (
-        <div>I am Logged in and this is about me</div>
-      ) 
-      : 
-      (
-        <main className="flex-row justify-center">
-          <img src="../images/football.jpg" alt="football" className="img" />
-          <div>
-            <h1 className="opacity-100 p-4">Premier Youth Football Camp</h1>
-          </div>
-          <div className="row justify-content-around m-4">
-            <div className="col col-5 justify-center border border-danger">  
-            <h2>Skills</h2>  
-              <ul className = 'list-group'>
-                <li className = 'list-group-item'>Catching</li>
-                <li className = 'list-group-item'>Route Running</li>
-                <li className = 'list-group-item'>Blocking</li>
-                <li className = 'list-group-item'>Film Study</li>
-              </ul>
-            </div>
-            <div className="col col-5 justify-center border border-info">
-              Professional Staff
+        <Camp />
 
+      )
+        :
+        (
+          <main className="flex-row justify-center">
+            <img src="../images/football.jpg" alt="football" className="img" />
+            <div>
+              <h1 className="opacity-100 p-4">Premier Youth Football Camp</h1>
             </div>
-          </div>
-        </main>
-      )}
-</div>
+            <div className="row justify-content-around m-4">
+              <div className="col col-5 justify-center border border-danger">
+                <h2>Skills</h2>
+                <ul className='list-group'>
+                  <li className='list-group-item'>Catching</li>
+                  <li className='list-group-item'>Route Running</li>
+                  <li className='list-group-item'>Blocking</li>
+                  <li className='list-group-item'>Film Study</li>
+                </ul>
+              </div>
+              <div className="col col-5 justify-center border border-info">
+                Professional Staff
+
+              </div>
+              <Camp />
+            </div>
+          </main>
+        )}
+    </div>
   );
 };
 
