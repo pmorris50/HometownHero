3b94b55fa6d0297921fe01aad9c54a5b8df15c24
jdettelback <jdettelback@gmail.com>
2023-02-16 21:12:02 -0500

    finished stripe

--- client/src/productsStore.js
+++ client/src/productsStore.js
@@ -1,50 +1,45 @@
 const productsArray = [
-    {
-      id: 1,
-      name: 'Registration Fee',
-      description:
-        'Camp Registration',
-      image: '#',
-      price: 50,
-      quantity: 20
-    },
-    {
-      id: 2,
-      name: 'Sweatshirt',
-      description:
-        'Sweatshirt with camp logo.',
-      image: '#',
-      price: 14.99,
-      quantity: 20
-    },
-    {
-      id: 3,
-      name: 'Long Sleeve Shirt',
-      description:
-        'Long-sleeve shirt with camp logo.',
-      image: '#',
-      price: 17.99,
-      quantity: 20
-    },
-    {
-      id: 4,
-      name: 'Mouthguard',
-      description:
-        'Mouthguard protection.',
-      image: '#',
-      price: 3.99,
-      quantity: 20
-    },
-    {
-      id: 5,
-      name: 'Baseball Cap',
-      description:
-        'Baseball Cap with camp logo.',
-      image: '#',
-      price: 12.99,
-      quantity: 20
-    },
-  ]
+  {
+    id: 'price_1McABdAoWLJeiXgFTAtI1KbZ',
+    name: "Registration Fee",
+    description: "Camp Registration",
+    image: "#",
+    price: 50,
+    quantity: 20,
+  },
+  {
+    id: 'price_1McAGQAoWLJeiXgFHE5iDvhe',
+    name: "Sweatshirt",
+    description: "Sweatshirt with camp logo.",
+    image: "#",
+    price: 14.99,
+    quantity: 20,
+  },
+  {
+    id: 'price_1McAHdAoWLJeiXgF7XKzxjGK',
+    name: "Long Sleeve Shirt",
+    description: "Long-sleeve shirt with camp logo.",
+    image: "#",
+    price: 17.99,
+    quantity: 20,
+  },
+  {
+    id: 'price_1McAITAoWLJeiXgFAotqPC4d',
+    name: "Mouthguard",
+    description: "Mouthguard protection.",
+    image: "#",
+    price: 3.99,
+    quantity: 20,
+  },
+  {
+    id: 'price_1McAF2AoWLJeiXgFFmA6NpHK',
+    name: "Baseball Cap",
+    description: "Baseball Cap with camp logo.",
+    image: "#",
+    price: 12.99,
+    quantity: 20,
+  },
+];
 
   function getProductData(id) {
     let productData = productsArray.find(product => product.id === id);
--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+var cors = require('cors');
+
 const stripe = require('stripe')('sk_test_51McA81AoWLJeiXgFmzKYRz2mpbQ7QtU0cJXQqqVA1RexaxhhBXvXCnXhmFSiXCi4zQPRk4hMlMECZcxdZdJTNaa000vlSHDcOF')
 
 app.use(express.urlencoded({ extended: false }));
@@ -28,32 +30,32 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// app.post("/checkout", async (req, res) => {
+app.post("/checkout", async (req, res) => {
   
-//   console.log(req.body);
-//   const items = req.body.items;
+  console.log(req.body);
+  const items = req.body.items;
 
-//   let lineItems = [];
-//   items.forEach((item) => {
-//     lineItems.push({
-//       price: item.id,
-//       quantity: item.quantity,
-//     });
-//   });
+  let lineItems = [];
+  items.forEach((item) => {
+    lineItems.push({
+      price: item.id,
+      quantity: item.quantity,
+    });
+  });
 
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: lineItems,
-//     mode: "payment",
-//     success_url: "http://localhost:3000/success",
-//     cancel_url: "http://localhost:3000/cancel",
-//   });
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+  });
 
-//   res.send(
-//     JSON.stringify({
-//       url: session.url,
-//     })
-//   );
-// });
+  res.send(
+    JSON.stringify({
+      url: session.url,
+    })
+  );
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
